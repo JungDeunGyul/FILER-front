b9942e9183d5f51f926df1a0b1221b8b5f8c27eb
JungDeunGyul <ask7384@gmail.com>
2024-03-11 15:40:57 +0900

    Chore: move firebase file to utils folder

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "../utils/firebase";
+import { auth, provider } from "../../utils/firebase";
 import useUserStore from "../store/userData";
 
 import axios from "axios";
