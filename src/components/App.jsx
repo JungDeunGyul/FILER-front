359a0592b1ec5cf22afc0004648c7133892ec51d
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-03-11 15:59:34 +0900

    refactor: simplify routes in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Login from "../components/Login";
 import Home from "../components/Home";
+import ExploreTeam from "../components/ExploreTeam";
+import MyTeam from "../components/MyTeam";
+import Login from "../components/Login";
+import useUserStore from "./store/userData";
 
 function App() {
+  const { userData } = useUserStore();
+
   return (
     <div>
       <div className="w-full h-full">
         <Routes>
-          <Route path="/" exact element={<Login />} />
-          <Route path="/home" exact element={<Home />} />
+          <Route
+            path="/"
+            element={userData ? <Navigate to="/home" /> : <Login />}
+          />
+          <Route
+            path="/home"
+            element={userData ? <Home /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/exploreteam"
+            element={userData ? <ExploreTeam /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/myteam"
+            element={userData ? <MyTeam /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </div>
