70d125ad44bb4055d7ac153533fd17893cc2ae86
JungDeunGyul <ask7384@gmail.com>
2024-03-22 16:02:40 +0900

    Feat: add FolderAccess modal

---
+++ src/components/Modal/FolderAccess.jsx
@@ -0,0 +1,12 @@
+export function FolderAccess({ setFolderAccessModalOpen }) {
+  const handleModalClick = () => {
+    setFolderAccessModalOpen(false);
+  };
+
+  return (
+    <div className="fixed w-full h-full z-10 bg-gray flex flex-col items-center justify-center p-4">
+      <div className="text-red-500">해당 폴더에 접근이 불가능합니다</div>
+      <div onClick={handleModalClick}>닫기</div>
+    </div>
+  );
+}
