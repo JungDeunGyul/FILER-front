978200cf5944a2b53c2d63539a4c0d124a518d88
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-03-27 09:35:34 +0900

    chore: update file icon URL settings using environment variables

--- src/utils/fileIconURL.js
+++ src/utils/fileIconURL.js
@@ -3,27 +3,27 @@ export const getFileIconUrl = (fileType) => {
     fileType ===
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   ) {
-    return "https://img.icons8.com/color/48/ms-excel.png";
+    return import.meta.env.VITE_EXCEL_ICON_URL;
   } else if (fileType === "application/pdf") {
-    return "https://img.icons8.com/officel/16/pdf.png";
+    return import.meta.env.VITE_PDF_ICON_URL;
   } else if (
     fileType === "image/jpeg" ||
     fileType === "image/gif" ||
     fileType === "image/bmp" ||
     fileType === "image/png"
   ) {
-    return "https://img.icons8.com/color/48/image.png";
+    return import.meta.env.VITE_IMAGE_ICON_URL;
   } else if (fileType === "audio/mpeg" || fileType === "audio/midi") {
-    return "https://img.icons8.com/ios/50/high-volume--v1.png";
+    return import.meta.env.VITE_AUDIO_ICON_URL;
   } else if (fileType === "application/zip") {
-    return "https://img.icons8.com/ios/50/zip.png";
+    return import.meta.env.VITE_ZIP_ICON_URL;
   } else if (
     fileType === "application/vnd.ms-powerpoint" ||
     fileType ===
       "application/vnd.openxmlformats-officedocument.presentationml.presentation"
   ) {
-    return "https://img.icons8.com/fluency/48/microsoft-powerpoint-2019.png";
+    return import.meta.env.VITE_POWERPOINT_ICON_URL;
   } else {
-    return "https://img.icons8.com/ios/50/code--v1.png";
+    return import.meta.env.VITE_DEFAULT_ICON_URL;
   }
 };
