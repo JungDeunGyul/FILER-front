11b65e2303e0a079fdef29118edd9329064aecc6
JungDeunGyul <ask7384@gmail.com>
2024-11-03 20:52:11 +0900

    Refactor: replace Zustand store with React Query in Home component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,11 @@
-import useUserStore from "../store/userData";
+import { useQueryClient } from "@tanstack/react-query";
+
 import ExploreTeam from "../ExploreTeam";
 import MyTeam from "../MyTeam";
 
 function Home() {
-  const { userData } = useUserStore();
+  const queryClient = useQueryClient();
+  const userData = queryClient.getQueryData(["userData"]);
 
   return (
     <div>{userData.teams.length === 0 ? <ExploreTeam /> : <MyTeam />}</div>
